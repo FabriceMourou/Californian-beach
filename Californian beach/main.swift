f70768969145bcc00b2e979e54668604bf7cda3e
FabriceMourou <shiimejiii@gmail.com>
2020-04-30 14:28:55 +0200

    Initial Commit

---
+++ Californian beach/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Californian beach
+//
+//  Created by Fabrice Mourou on 30/04/2020.
+//  Copyright © 2020 Fabrice Mourou. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
